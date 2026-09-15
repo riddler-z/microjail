bd6ca11b183e8a3c409e27a5c48698509a38d9fe
riddler-z <123775146+riddler-z@users.noreply.github.com>
2026-09-15 14:28:01 +0000

    milestone 1: fork, exec, and waitpid working

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h> // standard I/O 
+#include <sys/types.h> //defines system types, including *_t
+#include <unistd.h> //unix standard - gives fork() and getpid()
+#include <stdlib.h> //exit() lives in this library
+#include <sys/wait.h> //where macros for waitpid() live
+
+int main(void) {
+    printf("Before fork: I am process %d\n", getpid());
+    pid_t pid = fork();
+    /* fork() is a function that creates a new process
+    returns a pid_t variable named pid - returns whatever 
+    type the system uses to represent pid's
+    */
+
+    if (pid < 0){
+        perror("fork failed.");
+    } else if (pid == 0){
+        printf("PID is %d, fork returned %d to me\n", getpid(), pid);
+        char *args[] = {"/bin/echo", "hello from the child process", NULL};
+        if (execvp(args[0], args) == -1){ //exec always returns -1 on failure since its return type is int
+            perror("Execvp failed");
+            exit(1);
+        }
+    } else {
+        printf("PID is %d, fork returned %d to me\n", getpid(), pid);
+        
+        int status;
+        pid_t wpid = waitpid(pid, &status, 0); //capture return value and compare directly to -1
+
+        if (wpid == -1){
+            perror("Waitpid failed.");
+            exit(1);
+        }
+
+        if (WIFEXITED(status)) {
+            printf("Child exited normally with status %d\n", WEXITSTATUS(status));
+        } else {
+            printf("Child did not exit normally\n");
+        }
+    }
+    return 0; 
+}
